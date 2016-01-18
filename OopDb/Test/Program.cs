e54893201cece3bc2bfc91295646475b84bf55a7
EliandroLA <eliandro.email@gmail.com>
2016-01-17 22:44:30 -0200

    Melhorias

--- OopDb/Test/Program.cs
+++ OopDb/Test/Program.cs
@@ -1,4 +1,6 @@
 ﻿using OopDb;
+using System;
+using System.Reflection;
 
 namespace Test
 {
@@ -6,30 +8,49 @@ namespace Test
     {
         static void Main(string[] args)
         {
+            var t = new Test();
+
             var a = new User();
             a.Value = 9;
             a.Name = "123456";
-            a.Status = 'R';
+            a.Status = 'S';
 
-            Validator.ValidateObj(a);
+            t.User = a;
 
-            System.Console.ReadKey();
+            var db = new DbFramework(typeof(Test));
+            db.SetRules();
+            var script = db.GetScript(true);
+
+            Validator.ValidateObj(t);
+
+            Console.ReadKey();
 
         }
 
 
     }
 
-    [Alias("Teste")]
-    class User
+    class Test
     {
-        [GreaterThan(0), LessThan(10), DataType("float")]
-        public int Value { get; set; }
-        [MinLenght(6), MaxLenght(10)]
-        public string Name { get; set; }
-        [Check('R','S','W')]
-        public char Status { get; set; }
+        [IsKey]
+        public Guid Id { get; set; }
+        [IsForeign("IdUser", "Id")]
+        public User User { get; set; }
     }
 
-    
+    [Alias("Usuario"), Description(@"R=Runnig S=Stopped W=Waiting")]
+    class User
+    {
+        [IsKey, Alias("IdUser")]
+        public Guid Id { get; set; }
+        [GreaterThan(0), LessThan(10), Default(1)]
+        public int Value { get; set; }
+        [MinLenght(6), MaxLenght(10), IsRequired, IsUnique]
+        public string Name { get; set; }
+        [ToLower, Check('R', 'S', 'W')]
+        public char Status { get; set; }
+        [Precision(10,3)]
+        public double Teste { get; set; }
+
+    }
 }
